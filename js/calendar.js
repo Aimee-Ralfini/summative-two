139b9987c219aef33d66886bf7ea0c229b826cb7
Aimee Ralfini <107170054+Aimee-Ralfini@users.noreply.github.com>
2022-12-05 14:45:52 +1300

    Updated Dates page
    finished Front-end JS on Dates page.
    Still need to style litepicker calendar and link through the JS selection for the next page.

--- js/calendar.js
+++ js/calendar.js
@@ -27,3 +27,28 @@
 
 
   });
+
+  // picker.show();
+
+
+
+//number of people
+      var counter = 1;
+      var upCounterElement = document.querySelector(".up-counter");
+      var downCounterElement = document.querySelector(".down-counter");
+      var viewCounterElement = document.querySelector(".view-counter");
+
+      viewCounterElement.innerText = counter;
+      upCounterElement.addEventListener("click", function () {
+        counter = counter + 1;
+        viewCounterElement.innerText = counter;
+      });
+
+      downCounterElement.addEventListener("click", function () {
+        counter = counter - 1;
+        if (counter < 1) {
+          counter = 1;
+        }
+
+        viewCounterElement.innerText = counter;
+      });
